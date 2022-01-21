66d6986d2c49729b9fc5012cbdb1d2464ed038a9
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2022-01-21 11:26:48 +0300

    Check player position is valid

--- src/main/java/com/basketball/graphql/api/PlayerMutationResolver.java
+++ src/main/java/com/basketball/graphql/api/PlayerMutationResolver.java
@@ -6,6 +6,7 @@ import com.basketball.graphql.entity.Player;
 import com.basketball.graphql.repo.PlayerRepository;
 
 import java.util.Date;
+import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class PlayerMutationResolver implements GraphQLMutationResolver {
 
     private final PlayerRepository PlayerRepository;
+    private final String[] allowedPositions = {"Point guard(PG)", "Shooting guard(SG)", "Small forward(SF)", "Power forward(PF)", "Center(C)"};
 
     public Player createPlayer(PlayerDto PlayerDto) {
         return PlayerRepository.save(dtoToEntity(PlayerDto));
@@ -30,10 +32,22 @@ public class PlayerMutationResolver implements GraphQLMutationResolver {
     }
 
     private Player dtoToEntity(PlayerDto PlayerDto) {
-        Player Player = new Player();
-        Player.setName(PlayerDto.getName());
-        Player.setSurname(PlayerDto.getSurname());
-        Player.setPosition(PlayerDto.getPosition());
-        return Player;
+        String position = PlayerDto.getPosition();
+        boolean isPositionValid = false;
+        for (String allowedPosition : allowedPositions) {
+            if (position.equals(allowedPosition)) {
+                isPositionValid = true;
+                break;
+            }
+        }
+        if (isPositionValid) {
+            Player Player = new Player();
+            Player.setName(PlayerDto.getName());
+            Player.setSurname(PlayerDto.getSurname());
+            Player.setPosition(PlayerDto.getPosition());
+            return Player;
+        } else {
+            throw new IllegalArgumentException("Position is not valid");
+        }
     }
 }
